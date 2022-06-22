eacea479562a3b21138844fb1b7ad9adc58799b4
kinsun <kinsunlu@sina.com>
2022-06-22 11:07:19 +0800

    [更新]兼容Swift5

---
+++ KSSegmentedControl/Example/AppDelegate.swift
@@ -0,0 +1,166 @@
+//
+//  AppDelegate.swift
+//  KSSegmentedControl
+//
+//  Created by kinsun on 2018/8/15.
+//  Copyright © 2018年 kinsun. All rights reserved.
+//
+
+import UIKit
+
+@main
+open class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    open lazy var window: UIWindow? = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewController()
+        return window
+    }()
+
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
+open class MainViewController: UIViewController, UIScrollViewDelegate {
+    
+    open override func loadView() {
+        let view = _View()
+        view.scrollView.delegate = self
+        view.bar.segmented.didClickItemCallback = { [weak self] in
+            guard let view = self?.view as? _View else { return }
+            let x = view.bounds.size.width*CGFloat($0)
+            view.scrollView.setContentOffset(CGPoint(x: x, y: 0.0), animated: true)
+        }
+        self.view = view
+    }
+    
+    private var _isFirstLayout = true
+    
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard _isFirstLayout, view.window != nil, view.bounds != .zero else { return }
+        _isFirstLayout = false
+        /// 设定启动时默认在index = 2（第三页）
+        (view as? _View)?.scrollView.setContentOffset(CGPoint(x: view.bounds.width*2.0, y: 0.0), animated: false)
+    }
+
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        (view as? _View)?.bar.segmented.scrollViewDidScroll(scrollView: scrollView)
+    }
+    
+    @available(iOS 13.0, *)
+    open override var overrideUserInterfaceStyle: UIUserInterfaceStyle {
+        set { super.overrideUserInterfaceStyle = newValue }
+        get { .light }
+    }
+    
+}
+
+extension MainViewController {
+    
+    private class _Bar: UIView {
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        public let segmented: KSSegmentedControl = {
+            let segmented = KSSegmentedControl(items: ["列表", "年历", "月历", "日历"])
+            segmented.normalTextColor = UIColor(red: 255.0/255.0, green: 111.0/255.0, blue: 111.0/255.0, alpha: 1.0)
+            segmented.cornerRadius = 6.0
+            return segmented
+        }()
+        private let _line: UIView = {
+            let line = UIView()
+            line.backgroundColor = UIColor.init(red: 220.0/255.0, green: 220.0/255.0, blue: 220.0/255.0, alpha: 1.0)
+            line.isUserInteractionEnabled = false
+            return line
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            backgroundColor = .white
+            addSubview(segmented)
+            addSubview(_line)
+        }
+        
+        open var topMargin = CGFloat(0.0) {
+            didSet { setNeedsLayout() }
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            let windowSize = bounds.size
+            let windowWidth = windowSize.width
+            let windowHeight = windowSize.height
+            let viewW = min(CGFloat(segmented.items.count)*66.5, windowWidth-24.0)
+            let viewH = CGFloat(38.0)
+            segmented.frame = CGRect(x: (windowWidth-viewW)*0.5, y: (windowHeight-topMargin-viewH)*0.5+topMargin, width: viewW, height: viewH)
+            _line.frame = CGRect(x: 0.0, y: windowHeight-1.0, width: windowWidth, height: 1.0)
+        }
+        
+    }
+    
+    private class _View: UIView {
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        public let bar: _Bar
+        public let scrollView: UIScrollView = {
+            let scrollView = UIScrollView()
+            scrollView.isPagingEnabled = true
+            scrollView.showsHorizontalScrollIndicator = false
+            return scrollView
+        }()
+        private let _labels: [UILabel]
+
+        override init(frame: CGRect) {
+            let bar = _Bar()
+            self.bar = bar
+            let font = UIFont.boldSystemFont(ofSize: 280.0)
+            let color = UIColor(red: 255.0/255.0, green: 111.0/255.0, blue: 111.0/255.0, alpha: 1.0)
+            _labels = (0..<bar.segmented.items.count).map {
+                let label = UILabel()
+                label.font = font
+                label.textAlignment = .center
+                label.textColor = color
+                label.text = "\($0+1)"
+                return label
+            }
+            super.init(frame: frame)
+            backgroundColor = .white
+            _labels.forEach { scrollView.addSubview($0) }
+            addSubview(scrollView)
+            addSubview(bar)
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews();
+            let bounds = self.bounds
+            let windowSize = bounds.size
+            let windowWidth = windowSize.width
+            let top: CGFloat
+            if #available(iOS 11.0, *) {
+                top = safeAreaInsets.top
+            } else {
+                top = 0.0
+            }
+            bar.topMargin = top
+            bar.frame = CGRect(origin: .zero, size: CGSize(width: windowWidth, height: top+44.0))
+            scrollView.frame = bounds
+
+            let y = bar.frame.maxY
+            let h = windowSize.height-y
+            _labels.enumerated().forEach {
+                $0.1.frame = CGRect(x: windowWidth*CGFloat($0.0), y: y, width: windowWidth, height: h)
+            }
+            scrollView.contentSize = CGSize(width: windowWidth*CGFloat(_labels.count), height: 0.0)
+        }
+    }
+    
+}
--- KSSegmentedControl/KSSegmentedControl/KSSegmentedControl.swift
+++ KSSegmentedControl/KSSegmentedControl/KSSegmentedControl.swift
@@ -8,219 +8,201 @@
 
 import UIKit
 
-class KSSegmentedItemLayer: CATextLayer {
-    override init() {
-        super.init();
-        isWrapped = true;
-        alignmentMode = kCAAlignmentCenter;
-        contentsScale = UIScreen.main.scale;
-    }
+open class KSSegmentedControl: UIView {
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-class KSSegmentedControl: UIView {
-    
-    public private(set) var _items : [String]!
-    public var _didClickItem : ((_ index : Int, _ isAnimation : Bool)->Void)?
-    
-    private var _font : UIFont!
-    private var _normalTextColor : UIColor!
-    private var _highlightTextColor : UIColor!
-    private var _cornerRadius : CGFloat!
-    private var _selectedSegmentIndex : Int!
-
-    private var _highlightLayer : CALayer!
-    private var _maskLayer : CAShapeLayer!
-    private var _normalTextLayerArray : [KSSegmentedItemLayer]!
-    private var _highlightTextLayerArray : [KSSegmentedItemLayer]!
-    
-    public init(frame: CGRect, items: [String]) {
-        super.init(frame: frame);
-        _items = items;
-        __initView();
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func __initView() {
-        let layer = self.layer;
-        layer.masksToBounds = true;
-        
-        let highlightLayer = CALayer.init();
-        layer.addSublayer(highlightLayer);
-        _highlightLayer = highlightLayer;
-        
-        var normalTextLayerArray : [KSSegmentedItemLayer] = NSMutableArray.init() as! [KSSegmentedItemLayer];
-        var highlightTextLayerArray : [KSSegmentedItemLayer] = NSMutableArray.init() as! [KSSegmentedItemLayer];
-        for title in _items {
-            let normalTextLayer = KSSegmentedItemLayer.init();
-            normalTextLayer.string = title;
-            layer.insertSublayer(normalTextLayer, below: highlightLayer);
-            normalTextLayerArray.append(normalTextLayer);
-            
-            let highlightTextLayer = KSSegmentedItemLayer.init();
-            highlightTextLayer.string = title;
-            highlightLayer.addSublayer(highlightTextLayer);
-            highlightTextLayerArray.append(highlightTextLayer);
+    public let items: [String]
+    private let _highlightLayer = CALayer()
+    private let _maskLayer = CAShapeLayer()
+    private let _normalTextLayerArray: [_ItemLayer]
+    private let _highlightTextLayerArray: [_ItemLayer]
+    
+    public init(frame: CGRect = .zero, items: [String]) {
+        self.items = items
+        var normalTextLayerArray = [_ItemLayer]()
+        var highlightTextLayerArray = [_ItemLayer]()
+        for title in items {
+            let normalTextLayer = _ItemLayer()
+            normalTextLayer.string = title
+            normalTextLayerArray.append(normalTextLayer)
+            let highlightTextLayer = _ItemLayer()
+            highlightTextLayer.string = title
+            highlightTextLayerArray.append(highlightTextLayer)
         }
-        
-        _normalTextLayerArray = NSArray.init(array: normalTextLayerArray) as! [KSSegmentedItemLayer];
-        _highlightTextLayerArray = NSArray.init(array: highlightTextLayerArray) as! [KSSegmentedItemLayer];
-        
-        let maskLayer = CAShapeLayer.init();
-        highlightLayer.mask = maskLayer;
-        _maskLayer = maskLayer;
-        
-        font = UIFont.systemFont(ofSize: 17.0);
-        normalTextColor = UIColor.blue;
-        highlightTextColor = UIColor.white;
-        cornerRadius = 4.0;
+        _normalTextLayerArray = normalTextLayerArray
+        _highlightTextLayerArray = highlightTextLayerArray
+        super.init(frame: frame)
+        let layer = self.layer
+        layer.cornerRadius = 4.0
+        layer.masksToBounds = true
+        let pointSize = font.pointSize
+        let fontName = font.fontName as CFString
+        let fontRef = CGFont.init(fontName)
+        let normalTextColor = self.normalTextColor.cgColor
+        for i in 0..<items.count {
+            let normalTextLayer = normalTextLayerArray[i]
+            normalTextLayer.font = fontRef
+            normalTextLayer.fontSize = pointSize
+            normalTextLayer.foregroundColor = normalTextColor
+            layer.addSublayer(normalTextLayer)
+            
+            let highlightTextLayer = highlightTextLayerArray[i]
+            highlightTextLayer.font = fontRef
+            highlightTextLayer.fontSize = pointSize
+            _highlightLayer.addSublayer(highlightTextLayer)
+        }
+        _highlightLayer.backgroundColor = normalTextColor
+        _highlightLayer.mask = _maskLayer
+        layer.addSublayer(_highlightLayer)
     }
     
-    override func layoutSublayers(of layer: CALayer) {
-        _highlightLayer.frame = layer.bounds;
-        let size = layer.bounds.size;
-        let count = _items.count;
-        var viewX: CGFloat = 0.0;
-        let viewH: CGFloat = _font.lineHeight;
-        let viewY: CGFloat = (size.height-viewH)*0.5;
-        let viewW: CGFloat = size.width/CGFloat(count);
+    open override func layoutSublayers(of layer: CALayer) {
+        _highlightLayer.frame = layer.bounds
+        let size = layer.bounds.size
+        let count = items.count
+        var viewX = CGFloat(0.0)
+        let viewH = font.lineHeight
+        let viewY = (size.height-viewH)*0.5
+        let viewW = floor(size.width/CGFloat(count))
         
         for i in 0..<count {
-            let normalTextLayer = _normalTextLayerArray[i];
-            let highlightTextLayer = _highlightTextLayerArray[i];
-            let rect = CGRect.init(x: viewX, y: viewY, width: viewW, height: viewH);
-            normalTextLayer.frame = rect;
-            highlightTextLayer.frame = rect;
-            viewX = rect.maxX;
+            let normalTextLayer = _normalTextLayerArray[i]
+            let highlightTextLayer = _highlightTextLayerArray[i]
+            let rect = CGRect.init(x: viewX, y: viewY, width: viewW, height: viewH)
+            normalTextLayer.frame = rect
+            highlightTextLayer.frame = rect
+            viewX = rect.maxX
         }
         
-        let width = viewW;
-        let height = size.height;
-        let radius = _cornerRadius!;
-        let pi = CGFloat(Double.pi);
+        let width = viewW
+        let height = size.height
+        let radius = cornerRadius
+        let pi = CGFloat.pi
         
-        let path = UIBezierPath.init();
-        path.addArc(withCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle:pi, endAngle: -(pi*0.5), clockwise: true);
-        path.addArc(withCenter: CGPoint(x: width-radius, y: radius), radius: radius, startAngle:-(pi*0.5), endAngle: 0.0, clockwise: true);
-        path.addArc(withCenter: CGPoint(x: width-radius, y: height-radius), radius: radius, startAngle:0.0, endAngle: pi*0.5, clockwise: true);
-        path.addArc(withCenter: CGPoint(x: radius, y: height-radius), radius: radius, startAngle:pi*0.5, endAngle: pi, clockwise: true);
-        path.close();
-        _maskLayer.path = path.cgPath;
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle:pi, endAngle: -(pi*0.5), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: width-radius, y: radius), radius: radius, startAngle:-(pi*0.5), endAngle: 0.0, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: width-radius, y: height-radius), radius: radius, startAngle:0.0, endAngle: pi*0.5, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: radius, y: height-radius), radius: radius, startAngle:pi*0.5, endAngle: pi, clockwise: true)
+        path.close()
+        _maskLayer.path = path.cgPath
         
-        let selectedSegmentIndex = _selectedSegmentIndex!;
-        if _didClickItem != nil && selectedSegmentIndex != 0 {
-            _didClickItem!(selectedSegmentIndex, false);
-        }
+        var rect = _maskLayer.frame
+        rect.size.width = width
+        rect.origin.x = CGFloat(_selectedSegmentIndex)*width
+        _maskLayer.frame = rect
     }
     
     public func scrollViewDidScroll(scrollView : UIScrollView!) {
-        let x = scrollView.contentOffset.x;
-        let s = x/scrollView.contentSize.width;
-        let t = s*frame.size.width;
-        let maskLayer = _maskLayer!;
-        var rect = maskLayer.frame;
-        rect.origin.x = t;
-        CATransaction.begin();
-        CATransaction.setDisableActions(true);
-        maskLayer.frame = rect;
-        CATransaction.commit();
+        let x = scrollView.contentOffset.x
+        let s = x/scrollView.contentSize.width
+        let t = s*bounds.size.width
+        var rect = _maskLayer.frame
+        rect.origin.x = t
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        _maskLayer.frame = rect
+        CATransaction.commit()
         
-        let width = scrollView.frame.size.width;
-        let page = Int(ceil((x-width*0.5)/width));
-        _selectedSegmentIndex = page;
+        let width = scrollView.bounds.size.width
+        _selectedSegmentIndex = Int(ceil((x-width*0.5)/width))
     }
     
-    public var selectedSegmentIndex : Int! {
+    private var _selectedSegmentIndex = 0
+    
+    open var selectedSegmentIndex: Int {
         set {
-            if _selectedSegmentIndex != newValue {
-                _selectedSegmentIndex = newValue;
-                let maskLayer = _maskLayer!;
-                var rect = maskLayer.frame;
-                let t = frame.size.width/CGFloat(_items.count*newValue);
-                rect.origin.x = t;
-                maskLayer.frame = rect;
-            }
+            guard _selectedSegmentIndex != newValue else { return }
+            _selectedSegmentIndex = newValue
+            var rect = _maskLayer.frame
+            let t = bounds.size.width/CGFloat(items.count*newValue)
+            rect.origin.x = t
+            _maskLayer.frame = rect
         }
-        get {
-            return _selectedSegmentIndex;
-        }
+        get { _selectedSegmentIndex }
     }
     
-    public var cornerRadius : CGFloat! {
+    open var cornerRadius: CGFloat {
         set {
-            _cornerRadius = newValue;
-            layer.cornerRadius = newValue;
-            setNeedsDisplay();
+            layer.cornerRadius = newValue
+            setNeedsLayout()
         }
-        get {
-            return _cornerRadius;
-        }
+        get { layer.cornerRadius }
     }
     
-    public var font : UIFont! {
-        set {
-            _font = newValue;
-            let pointSize = font.pointSize;
-            let fontName = font.fontName as CFString;
-            let fontRef = CGFont.init(fontName);
-            for i in 0..<_items.count {
-                let normalTextLayer = _normalTextLayerArray[i];
-                normalTextLayer.font = fontRef;
-                normalTextLayer.fontSize = pointSize;
+    open var font = UIFont.systemFont(ofSize: 17.0) {
+        didSet {
+            let pointSize = font.pointSize
+            let fontName = font.fontName as CFString
+            let fontRef = CGFont.init(fontName)
+            for i in 0..<items.count {
+                let normalTextLayer = _normalTextLayerArray[i]
+                normalTextLayer.font = fontRef
+                normalTextLayer.fontSize = pointSize
                 
-                let highlightTextLayer = _highlightTextLayerArray[i];
-                highlightTextLayer.font = fontRef;
-                highlightTextLayer.fontSize = pointSize;
+                let highlightTextLayer = _highlightTextLayerArray[i]
+                highlightTextLayer.font = fontRef
+                highlightTextLayer.fontSize = pointSize
             }
-        }
-        get {
-            return _font;
         }
     }
     
-    public var normalTextColor : UIColor! {
-        set {
-            _normalTextColor = newValue;
-            let color =  newValue.cgColor;
+    open var normalTextColor = UIColor.blue {
+        didSet {
+            let color = normalTextColor.cgColor
             for normalTextLayer in _normalTextLayerArray {
-                normalTextLayer.foregroundColor = color;
+                normalTextLayer.foregroundColor = color
             }
-            _highlightLayer.backgroundColor = color;
-        }
-        get {
-            return _normalTextColor;
-        }
-    }
-    public var highlightTextColor : UIColor! {
-        set {
-            _highlightTextColor = newValue;
-            let color =  newValue.cgColor;
-            for highlightTextLayer in _highlightTextLayerArray {
-                highlightTextLayer.foregroundColor = color;
-            }
-        }
-        get {
-            return _highlightTextColor;
+            _highlightLayer.backgroundColor = color
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event);
-        if _didClickItem != nil {
-            let touch = touches.first!;
-            let location = touch.location(in: self);
-            if bounds.contains(location) {
-                let page = location.x/(frame.size.width/CGFloat(_items.count));
-                let index = Int(page);
-                _selectedSegmentIndex = index;
-                _didClickItem!(index, true);
+    open var highlightTextColor = UIColor.white {
+        didSet {
+            let color = highlightTextColor.cgColor
+            for highlightTextLayer in _highlightTextLayerArray {
+                highlightTextLayer.foregroundColor = color
             }
         }
     }
+    
+    open var didClickItemCallback : ((Int) -> Void)?
+    
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        guard let c = didClickItemCallback, let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        guard bounds.contains(location) else { return }
+        let index = Int(location.x/(bounds.size.width/CGFloat(items.count)))
+        c(index)
+    }
+}
+
+extension KSSegmentedControl {
+    
+    private class _ItemLayer: CATextLayer {
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override init() {
+            super.init()
+            isWrapped = true
+            alignmentMode = .center
+            contentsScale = UIScreen.main.scale
+        }
+        
+        override init(layer: Any) {
+            super.init(layer: layer)
+            isWrapped = true
+            alignmentMode = .center
+            contentsScale = UIScreen.main.scale
+        }
+        
+    }
+    
 }
